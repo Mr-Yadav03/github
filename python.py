03f74f91bc9deb35384a922f5480dde21ecb10e0
Mr-Yadav03 <vikritji@gmail.com>
2023-12-01 09:54:17 +0530

    arithmatic operation2

--- python.py
+++ python.py
@@ -3,6 +3,13 @@ print("Git tutorial wala");
 a = 100;
 print("value of a = ",a);
 b = a
-print("value of b = ",b)
+print("value of b = ",b);
 c= a+b
-print("value of C = ",c)
+print("value of C = ",c);
+d=a//b
+print("value of d = ",d);
+# E-mc^2
+m=10
+c=300000
+E=m*c**2
+print("E=mc^2 where m = 10, is = ",E)
