50e01be45a2c48c2f465497d53be3fc3a1d3e8de
Mr-Yadav03 <vikritji@gmail.com>
2023-12-03 11:32:58 +0530

    assignment arithmetic2

--- arithmetic.py
+++ arithmetic.py
@@ -13,3 +13,13 @@ print("multiplication : ",e)
 print("square root : ",f)
 print("division : ",g)
 print("non floating : ",h)
+# average of numbers
+a1 =int(input("Enter the first number : "))
+b1 =int(input("Enter the second number : "))
+avg = (a1+b1)//2;
+print("average of two numbers : ",avg)
+radius = float(input("Enter radius value : "))
+area = 3.1412*radius**2
+circumference = 2*3.1412*radius
+print("area of circle: ",area)
+print("circumference of circle: ",circumference)
---
+++ arithmetic2.py
@@ -0,0 +1,49 @@
+# # This file has some more basic arithmetic examples which looks a little different
+# a = 10
+# b = 3
+# a+=b
+# print("a+=b: ",a)
+# a-=b
+# print("a-=b: ",a)
+# a*=b
+# print("a*=b: ",a)
+# a/=b
+# print("a/=b: ",a)
+# a//=b
+# print("a//=b: ",a)
+# a**=b
+# print("a**=b: ",a)
+# a%=b
+# print("a%=b: ",a)
+
+# # Relational operators
+# # '<'   '<='    '>'    '>='    '=='    '!='
+# a1 = 10
+# b1 = 20
+# print(a1>b1)
+# print(a1<b1)
+# print(a1<=b1)
+# print(a1>=b1)
+# print(a1==b1)
+# print(a1!=b1)
+
+# # Logical operator
+# # AND (and) where T and T = T
+# a2=10
+# b2=20
+# c2=30
+# print(a2<b2 and b2<c2)
+# # Logical OR (or)
+# print(a2>b2 or b2<c2)
+# # logical NOT (not)
+# print(not(a2<b2))
+
+# Bit wise operator
+a = 10
+b = 4
+
+print(a&b) # AND operator
+print(a|b) # OR operator
+print(a^b) # XOR operator
+print(b<<2) # Left Shift operator (4x4)
+print(b>>2) # Right Shift operator (4/(2x2))
