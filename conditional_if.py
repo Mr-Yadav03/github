26e7d2ca0c48cce7e8d50a0cdfe5afb3200e7f2b
Mr-Yadav03 <vikritji@gmail.com>
2023-12-05 13:00:40 +0530

    conditional_if

---
+++ conditional_if.py
@@ -0,0 +1,24 @@
+# # Conditional if practice
+# # > <   >=  <=  ==  !=
+# # If statement
+# a = input(f"x: ")
+# b = input(f"y: ")
+# if a <  b :
+#     print("Y is greater than X")
+# else :
+#     print("X is greater than Y")
+
+# age = int(input(print("Enter the age: ")))
+# if(age>=18) :
+#     print("Your age is: ",age," Please! Vote for nation")
+# else :
+#     print("Your age is: ",age,", You are unable to vote")
+
+# Python code to check entered character is alphabet or digit
+ch= input(print("Enter the character: "))
+if(ch.isalpha()):
+    print("Character is alphabet: ")
+if(ch.isdigit()):
+    print("Character is digit: ")
+if(ch.isspace()):
+    print("Character is space: ")
